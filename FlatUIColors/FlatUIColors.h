36417cbcadd8c338ef52b965aa1758695f09cbd0
Naoki Morita <namorit@gmail.com>
2015-08-02 04:49:20 +0900

    Carthage compatible

---
+++ FlatUIColors/FlatUIColors.h
@@ -0,0 +1,19 @@
+//
+//  FlatUIColors.h
+//  FlatUIColors
+//
+//  Created by MORITANAOKI on 2015/08/02.
+//  Copyright (c) 2015年 FlatUIColors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlatUIColors.
+FOUNDATION_EXPORT double FlatUIColorsVersionNumber;
+
+//! Project version string for FlatUIColors.
+FOUNDATION_EXPORT const unsigned char FlatUIColorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlatUIColors/PublicHeader.h>
+
+
